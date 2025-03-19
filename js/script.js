d18a1405562a205985749f15a7c73877e0832e24
amur <amyrkatyk@gmail.com>
2025-03-19 17:58:12 +0500

    for home

--- js/script.js
+++ js/script.js
@@ -2,10 +2,44 @@ const reelsSwiper = new Swiper('.reels-swiper', {
     direction: 'vertical',
 })
 
-console.log($('.teaser'))
+const clientWidth = window.innerWidth;
 
-$(function() {
+$(function () {
     $.scrollify({
         section: ".scrollify",
+        sectionName: "section-name",
+        interstitialSection: "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1400,
+        offset: 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll: true,
+        before: function () { },
+        after: function () { },
+        afterResize: function () { },
+        afterRender: function () { }
     });
 })
+
+document.addEventListener('touchstart', function(e) {
+    if (reelsSwiper.el.contains(e.target)) {
+        $.scrollify.disable()
+        return;
+    }
+    $.scrollify.enable();
+})
+
+const svg = document.querySelector('svg.teaser__img');
+let width = svg.clientWidth;
+let height = svg.clientHeight;
+let coeff = 0.69;
+
+if (clientWidth <= 1400) {
+    svg.style.height = `${width * coeff}px`;
+} else if (clientWidth <= 1200) {
+    console.log(width)
+}
