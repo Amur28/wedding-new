1a99b8cc2b1c98a3a45c1e07c437bd4b62e5830b
amur <amyrkatyk@gmail.com>
2025-03-19 16:39:35 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const reelsSwiper = new Swiper('.reels-swiper', {
+    direction: 'vertical',
+})
+
+console.log($('.teaser'))
+
+$(function() {
+    $.scrollify({
+        section: ".scrollify",
+    });
+})
