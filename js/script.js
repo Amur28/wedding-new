ca68be77ca46237074eb76fb03e1552147827451
amur <amyrkatyk@gmail.com>
2025-03-20 13:30:38 +0500

    fix swiper

--- js/script.js
+++ js/script.js
@@ -4,34 +4,58 @@ const reelsSwiper = new Swiper('.reels-swiper', {
 
 const clientWidth = window.innerWidth;
 
-// $(function () {
-//     $.scrollify({
-//         section: ".scrollify",
-//         sectionName: "section-name",
-//         interstitialSection: "",
-//         easing: "easeOutExpo",
-//         scrollSpeed: 1400,
-//         offset: 0,
-//         scrollbars: true,
-//         standardScrollElements: "",
-//         setHeights: true,
-//         overflowScroll: true,
-//         updateHash: true,
-//         touchScroll: true,
-//         before: function () { },
-//         after: function () { },
-//         afterResize: function () { },
-//         afterRender: function () { }
-//     });
-// })
+$(function () {
+    $.scrollify({
+        section: ".scrollify",
+        sectionName: "section-name",
+        interstitialSection: "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1400,
+        offset: 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll: true,
+        before: function () { },
+        after: function () { },
+        afterResize: function () { },
+        afterRender: function () { }
+    });
+})
 
 // document.addEventListener('touchstart', function(e) {
 //     if (reelsSwiper.el.contains(e.target)) {
 //         $.scrollify.disable()
-//         return;
+//         if (reelsSwiper.isEnd) {
+//             $.scrollify.enable();
+//         }
 //     }
-//     $.scrollify.enable();
 // })
+
+document.addEventListener("touchstart", e => y = e.changedTouches[0].clientY);
+document.addEventListener("touchmove", e => {
+    let dir = e.changedTouches[0].clientY - y;
+    let isSwiper = reelsSwiper.el.contains(e.target)
+    let length = reelsSwiper.slides.length;
+    if (dir < 0 && isSwiper) {
+        $.scrollify.disable()
+        if (reelsSwiper.activeIndex == length - 1) {
+            $.scrollify.enable()
+        }
+        return
+    }
+
+    if (dir > 0 && isSwiper) {
+        if (reelsSwiper.activeIndex == length - 1) {
+            $.scrollify.disable()
+        }
+        if (reelsSwiper.activeIndex == 0) {
+            $.scrollify.enable()
+        }
+    }
+});
 
 const svg = document.querySelector('svg.teaser__img');
 let width = svg.clientWidth;
@@ -40,6 +64,4 @@ let coeff = 0.69;
 
 if (clientWidth <= 1400) {
     svg.style.height = `${width * coeff}px`;
-} else if (clientWidth <= 1200) {
-    console.log(width)
 }
