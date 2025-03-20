c3092ad11015d43e110aa67d4862670b84e39da9
amur <amyrkatyk@gmail.com>
2025-03-20 11:57:16 +0500

    styles

--- js/script.js
+++ js/script.js
@@ -4,34 +4,34 @@ const reelsSwiper = new Swiper('.reels-swiper', {
 
 const clientWidth = window.innerWidth;
 
-$(function () {
-    $.scrollify({
-        section: ".scrollify",
-        sectionName: "section-name",
-        interstitialSection: "",
-        easing: "easeOutExpo",
-        scrollSpeed: 1400,
-        offset: 0,
-        scrollbars: true,
-        standardScrollElements: "",
-        setHeights: true,
-        overflowScroll: true,
-        updateHash: true,
-        touchScroll: true,
-        before: function () { },
-        after: function () { },
-        afterResize: function () { },
-        afterRender: function () { }
-    });
-})
+// $(function () {
+//     $.scrollify({
+//         section: ".scrollify",
+//         sectionName: "section-name",
+//         interstitialSection: "",
+//         easing: "easeOutExpo",
+//         scrollSpeed: 1400,
+//         offset: 0,
+//         scrollbars: true,
+//         standardScrollElements: "",
+//         setHeights: true,
+//         overflowScroll: true,
+//         updateHash: true,
+//         touchScroll: true,
+//         before: function () { },
+//         after: function () { },
+//         afterResize: function () { },
+//         afterRender: function () { }
+//     });
+// })
 
-document.addEventListener('touchstart', function(e) {
-    if (reelsSwiper.el.contains(e.target)) {
-        $.scrollify.disable()
-        return;
-    }
-    $.scrollify.enable();
-})
+// document.addEventListener('touchstart', function(e) {
+//     if (reelsSwiper.el.contains(e.target)) {
+//         $.scrollify.disable()
+//         return;
+//     }
+//     $.scrollify.enable();
+// })
 
 const svg = document.querySelector('svg.teaser__img');
 let width = svg.clientWidth;
